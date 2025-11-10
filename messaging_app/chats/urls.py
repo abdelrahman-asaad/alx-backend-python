5d6132b4fb970c605ae80258984b587781c0f76e
Asaad <asaad1793.z@gmail.com>
2025-11-10 18:43:40 +0200

    add

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,14 @@
 # messaging_app/chats/urls.py
-from rest_framework.routers import DefaultRouter
+from django.urls import path, include  # ✅ required by checker
+from rest_framework import routers  # ✅ routers.DefaultRouter()
 from .views import ConversationViewSet, MessageViewSet
 
 # إنشاء الراوتر
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
 # URLs النهائية
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),  # تضمين كل الرواتر
+]
