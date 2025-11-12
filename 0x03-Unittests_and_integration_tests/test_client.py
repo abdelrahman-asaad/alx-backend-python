9f9696af4e56e66af3f532e5d61070b554fe6936
Asaad <asaad1793.z@gmail.com>
2025-11-12 13:41:19 +0200

    add

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -142,9 +142,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         repos = client.public_repos(license="apache-2.0")
         self.assertEqual(repos, self.apache2_repos)
 
-
-
-@parameterized_class([
+# ===== TASK 9 =====
+    @parameterized_class([
 
 
     {
@@ -154,14 +153,14 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         'apache2_repos': TEST_PAYLOAD[0][3],
     },
 ])
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Integration tests for GithubOrgClient"""
+    class TestIntegrationGithubOrgClient(unittest.TestCase):
+        """Integration tests for GithubOrgClient"""
 
-    @classmethod
-    def setUpClass(cls):
-        """Set up class for integration tests"""
-        cls.get_patcher = patch('requests.get')
-        cls.mock_get = cls.get_patcher.start()
+        @classmethod
+        def setUpClass(cls):
+            """Set up class for integration tests"""
+            cls.get_patcher = patch('requests.get')
+            cls.mock_get = cls.get_patcher.start()
 
         def side_effect(url):
             """Side effect for requests.get mock"""
