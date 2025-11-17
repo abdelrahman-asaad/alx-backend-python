134cb17ca40ace4be0ebc6e00a450112ef85120e
Asaad <asaad1793.z@gmail.com>
2025-11-17 20:29:23 +0200

    add chats views

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,21 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+#PageNumberPagination is a built-in pagination class in Django REST Framework that divides large result
+# sets into manageable pages.
 
 class MessagePagination(PageNumberPagination):
-    page_size = 20  # عدد العناصر لكل صفحة
-    page_size_query_param = "page_size"  # يسمح بتغيير عدد الرسائل عن طريق query param
-    max_page_size = 100  # الحد الأقصى المسموح به
+    page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response({
+            "count": self.page.paginator.count,  
+            "next": self.get_next_link(),
+            "previous": self.get_previous_link(),
+            "results": data,
+        })
+# Custom pagination class to paginate messages in conversations.
+# It extends PageNumberPagination to set a default page size of 20 messages per page.
+
+#get_paginated_response is overridden to customize the structure of the paginated response,
+# including total count, next and previous links, and the actual results.
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -80,6 +80,10 @@ TEMPLATES = [
     ],
 }'''
 
+
+
+
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
@@ -88,6 +92,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 from datetime import timedelta
