51406facfb77b71b44c9e79246a323f90f7f415c
Asaad <asaad1793.z@gmail.com>
2025-11-17 19:17:32 +0200

    jwt token modifiy

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -79,9 +79,11 @@ TEMPLATES = [
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }'''
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
