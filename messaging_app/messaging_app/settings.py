b7a9e11712638157d66d686dfe03c69a82240c40
Asaad <asaad1793.z@gmail.com>
2025-11-17 19:09:48 +0200

    jwt token modifiy

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 # ✅ Django REST Framework Configuration
-REST_FRAMEWORK = {
+'''REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
@@ -78,7 +78,23 @@ REST_FRAMEWORK = {
         # ممكن تضيف JWT بعدين زي:
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
+}'''
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
 }
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
